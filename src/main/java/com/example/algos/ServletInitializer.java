b263baa727ed60ac264d8a69db503fa3fb21f740
777 <goszakupki.aktiv@gmail.com>
2025-11-02 23:18:11 +0300

    start

---
+++ src/main/java/com/example/algos/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.algos;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DemoApplication.class);
+    }
+
+}
