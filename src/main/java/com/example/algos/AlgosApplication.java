fcd544ea0367ae66a32bf3dbfa41fd1726a491a0
777 <goszakupki.aktiv@gmail.com>
2025-11-03 16:24:13 +0300

    buble sort

--- src/main/java/com/example/algos/AlgosApplication.java
+++ src/main/java/com/example/algos/AlgosApplication.java
@@ -1,5 +1,6 @@
 package com.example.algos;
 
+import com.example.algos.polindrom.BubleSort;
 import com.example.algos.polindrom.Palindrome;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,16 +17,20 @@ public class AlgosApplication {
 
             System.out.println("Выберите число для действия:");
             System.out.println("1 палиндром");
-            System.out.println("2 выход");
+            System.out.println("2 пузырьковая сортировка массива");
+            System.out.println("0 выход");
             Scanner scanner = new Scanner(System.in);
             String scan = scanner.nextLine();
             try {
                 Integer number = Integer.parseInt(scan);
-                if (number == 1) {
+                if (number == 0) {
+                    break;
+                } else if (number == 1) {
                     new Palindrome().poli();
                 } else if (number == 2) {
-                    break;
+                    new BubleSort().bubleSort();
                 }
+
             } catch (NumberFormatException e) {
                 System.out.println("Ошибка! Введите число 1 или 2.");
             }
